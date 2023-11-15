f7cfe33c982fd65992fdb5f1dcc314c984fec61b
Mona <monabadee@gmail.com>
2023-11-15 21:56:26 +0300

    Complete Artihmetic Lab

--- index.js
+++ index.js
@@ -1,2 +1,27 @@
 
 // Write your code here
+
+// Assignment 1
+let num1 = 2;
+let num2 = 31;
+let multiply = num1 * num2;
+console.log(multiply);
+
+
+// Assignment 2
+let range = 100;
+let random = Math.floor(Math.random() * range) + 1;
+console.log(random); 
+
+
+// Assignment 3
+let num3 = 14;
+let num4 = 5;
+let mod = num3 % num4;
+console.log(mod);
+
+
+// Assignment 4
+let numberSet = [10, 5, 20, 15];
+let max = Math.max(...numberSet);
+
